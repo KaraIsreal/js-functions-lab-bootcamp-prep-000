468c1b417daeeced9eeca6e6eb468b237eb1bf92
Kara Isreal <isreal.kara92@gmail.com>
2018-08-29 19:51:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 function happyHolidays() {
-  return('Happy holidays!')
+  return(`Happy holidays!`)
 }
 
 function happyHolidaysTo(name) {
-  return('Happy holidays, ${name}')
+  return(`Happy holidays, ${name}`)
 }
 
 function happyHolidayTo(holiday, name) {
-  return('${holiday}, ${name}!')
+  return(`${holiday}, ${name}!`)
 }
 
 function holidayCountdown(holiday, days) {
   return(`It's ${days} days until ${holiday}!`)
 }
-
